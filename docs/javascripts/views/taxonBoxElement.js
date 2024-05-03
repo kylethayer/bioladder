e262c1c02b5d1a5f3c113b2e8e99c4478eff0c30
Kyle Thayer <kyle.thayer@gmail.com>
2024-05-02 23:47:58 -0700

    refactor TaxonBoxElements to have named options for better code clarity

--- docs/javascripts/views/taxonBoxElement.js
+++ docs/javascripts/views/taxonBoxElement.js
@@ -1,13 +1,25 @@
 
 class TaxonBoxElement{
-    constructor(elementName, className, 
-        refreshPreTransitionFn, postTransitionFn, initialTransitionFeatures){
-        this.elementName = elementName
-        this.className = className
-        this.refreshPreTransitionFn = refreshPreTransitionFn
-        this.postTransitionFn = postTransitionFn
-        if(initialTransitionFeatures){
-            this.initialTransitionFeatures = initialTransitionFeatures
+    // constructor options can have
+    // - elementName - the HTML tag name to create
+    // - className - the class name to give these elements
+    // - refreshPreTransitionFn - settings to set on elements before transition (e.g., permenent, non-transition settings)
+    // - postTransitionFn - settings to transition to for new state
+    // - initialTransitionFeatures - settings to start with for new elements to transition from on first appearance
+    constructor(options){
+        this.elementName = options.elementName
+        this.className = options.className
+
+        if(options.refreshPreTransitionFn){
+            this.refreshPreTransitionFn = options.refreshPreTransitionFn
+        }else{
+            this.refreshPreTransitionFn = selection => selection
+        }
+
+        this.postTransitionFn = options.postTransitionFn
+
+        if(options.initialTransitionFeatures){
+            this.initialTransitionFeatures = options.initialTransitionFeatures
         }else{
             this.initialTransitionFeatures = this.postTransitionFn
         }
@@ -28,80 +40,80 @@ class TaxonBoxElement{
     }
 }
 
+
+// Create the elements that will be part of the taxon boxes
 const taxonBoxElements = [
     //background (with box shadow)
-    new TaxonBoxElement('rect', 'taxon-box-background',
-        selection => selection,
-        selection => 
-            selection
-                .attr('width', (d) => d.width)
-                .attr('height', (d) => d.height)
-                .attr('style', (d) => {
-                    let popularity = d.taxon.popularity ? d.taxon.popularity : 0;
-                    let minShadowWidth = popularity / 100.0 * 0.3;
-                    let maxShadowWidth = popularity / 100.0 * 0.9;
-                    return `filter: drop-shadow(0 ${minShadowWidth}em ${maxShadowWidth}em rgba(0,0,0,0.8));`
-                })
+    new TaxonBoxElement({
+        elementName: 'rect', 
+        className: 'taxon-box-background',
+        postTransitionFn: selection => selection
+            .attr('width', (d) => d.width)
+            .attr('height', (d) => d.height)
+            .attr('style', (d) => {
+                let popularity = d.taxon.popularity ? d.taxon.popularity : 0;
+                let minShadowWidth = popularity / 100.0 * 0.3;
+                let maxShadowWidth = popularity / 100.0 * 0.9;
+                return `filter: drop-shadow(0 ${minShadowWidth}em ${maxShadowWidth}em rgba(0,0,0,0.8));`
+            })
         
-    ),
+    }),
     //taxonLabel rectangle
-    new TaxonBoxElement('rect', 'taxon-label-rect',
-        selection => selection,
-        selection => 
-            selection
-                .attr('width', (d) => d.width)
-                .attr('height', (d) => d.labelHeight)
-                .attr('fill', 'orange')
+    new TaxonBoxElement({
+        elementName: 'rect', 
+        className: 'taxon-label-rect',
+        postTransitionFn: selection => selection
+            .attr('width', (d) => d.width)
+            .attr('height', (d) => d.labelHeight)
+            .attr('fill', 'orange')
         
-    ),
+    }),
     // label text
-    new TaxonBoxElement('text', 'taxon-label-text',
-        selection => 
-            selection
-                .text((d) => d.taxon.name)
-                .attr('dominant-baseline', 'central')
-                .attr('text-anchor', 'middle')
+    new TaxonBoxElement({
+        elementName: 'text', 
+        className: 'taxon-label-text',
+        refreshPreTransitionFn: selection => selection
+            .text((d) => d.taxon.name)
+            .attr('dominant-baseline', 'central')
+            .attr('text-anchor', 'middle')
         ,
-        selection => 
-            selection
-                .attr('x', (d) => d.width / 2)
-                .attr('y', (d) => d.labelHeight / 2)
+        postTransitionFn: selection => selection
+            .attr('x', (d) => d.width / 2)
+            .attr('y', (d) => d.labelHeight / 2)
         
-    ),
+    }),
     // image (only for those that have images)
-    new TaxonBoxElement('image', 'taxon-wikipedia-img',
-        selection => 
-            selection
-                .attr('class', 'taxon-wikipedia-img')
-                .attr('href', (d) => d.taxon.getPreviewImage())
-                .attr('hidden', (d) => d.taxon.getPreviewImage() && d.isOpen ? null: true)
+    new TaxonBoxElement({
+        elementName: 'image', 
+        className: 'taxon-wikipedia-img',
+        refreshPreTransitionFn: selection => selection
+            .attr('class', 'taxon-wikipedia-img')
+            .attr('href', (d) => d.taxon.getPreviewImage())
+            .attr('hidden', (d) => d.taxon.getPreviewImage() && d.isOpen ? null: true)
         ,
-        selection => 
-            selection
-                .attr('x', 10)
-                .attr('y', (d) => d.labelHeight +10)
-                .attr('style', (d) => {
-                    return `width:${d.width / 2}px; height:${d.height / 2}px;`
-                })
+        postTransitionFn: selection => selection
+            .attr('x', 10)
+            .attr('y', (d) => d.labelHeight +10)
+            .attr('style', (d) => {
+                return `width:${d.width / 2}px; height:${d.height / 2}px;`
+            })
         ,
-        selection =>  // initial transition features
-            selection
-                .attr('x', 10)
-                .attr('y', (d) => d.labelHeight +10)
-                .attr('style', (d) => {
-                    return `width:${0}px; height:${0}px;`
-                })
+        initialTransitionFeatures: selection => selection
+            .attr('x', 10)
+            .attr('y', (d) => d.labelHeight +10)
+            .attr('style', (d) => {
+                return `width:${0}px; height:${0}px;`
+            })
         
-    ),
+    }),
     // outline
-    new TaxonBoxElement('rect', 'taxon-box-outline',
-        selection => selection,
-        selection => 
-            selection
-                .attr('width', (d) => d.width)
-                .attr('height', (d) => d.height)
-        
-    ),
+    new TaxonBoxElement({
+        elementName: 'rect', 
+        className: 'taxon-box-outline',
+        postTransitionFn: selection => selection
+            .attr('width', (d) => d.width)
+            .attr('height', (d) => d.height)
+    }),
 ]
 
 export {taxonBoxElements}
