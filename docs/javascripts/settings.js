3dd1b592ea7018b121241c6359dfa1cbb4753487
Kyle Thayer <kyle.thayer@gmail.com>
2024-05-03 17:53:12 -0700

    depend on deployed version of data

--- docs/javascripts/settings.js
+++ docs/javascripts/settings.js
@@ -1,3 +1,4 @@
-const data_url_base = "http://localhost:8000/bioladder-data/"
+//const data_url_base = "http://localhost:8000/bioladder-data/"
+const data_url_base = "https://kylethayer.github.io/bioladder-data/"
 
 export default {data_url_base}
