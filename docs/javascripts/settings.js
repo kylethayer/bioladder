8baa9939f7d2e38312838d7a8b3a8b0e6900646d
Kyle Thayer <kyle.thayer@gmail.com>
2024-05-05 22:05:53 -0700

    update data project to use the docs directory

--- docs/javascripts/settings.js
+++ docs/javascripts/settings.js
@@ -1,4 +1,4 @@
-//const data_url_base = "http://localhost:8000/bioladder-data/"
+//const data_url_base = "http://localhost:8000/bioladder-data/docs/"
 const data_url_base = "https://kylethayer.github.io/bioladder-data/"
 
 export default {data_url_base}
